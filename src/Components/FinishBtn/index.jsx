72527c82eb3727514b515ac0061dfcf504f93b30
Gabriel Gomes Rodrigues <gabrielbj99@gmail.com>
2022-07-19 18:36:31 -0300

    Iniciando requisito 44

--- src/Components/FinishBtn/index.jsx
+++ src/Components/FinishBtn/index.jsx
@@ -5,7 +5,42 @@ import { useHistory } from 'react-router-dom';
 
 const FinishBtn = (props) => {
   const history = useHistory();
+  const location = 
   const { ingredientsCheck, recipeInfo } = props;
+
+  const updateRecipes = () => {
+    const today = new Date();
+    const doneDate = `${today.getDate()}/${today.getMonth()}/${today.getFullYear()}`;
+
+    if (history.location.pathname)
+    const {
+      idMeal: id,
+      strArea: nationality,
+      strCategory: category,
+      strMeal: name,
+      strMealThumb: image,
+      strTags: tags,
+    } = recipeInfo;
+    const recipe = {
+      id,
+      nationality,
+      category,
+      alcoholicOrNot: '',
+      name,
+      image,
+      doneDate,
+      tags,
+      type: 'food',
+    };
+
+    const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+    if (doneRecipes) {
+      localStorage.setItem('doneRecipes', JSON.stringify([...doneRecipes, recipe]));
+    } else {
+      localStorage.setItem('doneRecipes', JSON.stringify([recipe]));
+    }
+    history.push('/done-recipes');
+  };
 
   const counter = Object.keys(recipeInfo).filter((a) => a.includes('strIngredient'))
     .map((info, index) => {
@@ -34,7 +69,7 @@ const FinishBtn = (props) => {
     <button
       data-testid="finish-recipe-btn"
       type="button"
-      onClick={ () => history.push('/done-recipes') }
+      onClick={ updateRecipes }
     >
       Finish Recipe
     </button>
