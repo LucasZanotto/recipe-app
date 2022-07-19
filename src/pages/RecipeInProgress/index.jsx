988cf0a64efae031a34543ed15862aa564b5c03f
MatheusTostes <matheus_tostes@hotmail.com>
2022-07-19 05:32:39 -0300

    37 ao 43 sem testes, refatorar (complexidade)

--- src/pages/RecipeInProgress/index.jsx
+++ src/pages/RecipeInProgress/index.jsx
@@ -3,10 +3,23 @@ import { useLocation } from 'react-router-dom';
 import Context from '../../context/context';
 import getDrinkDetails from '../../services/api/getDrinkDetails';
 import getFoodDetails from '../../services/api/getFoodDetails';
+import imageComp from '../../images/shareIcon.svg';
+import whiteHeart from '../../images/whiteHeartIcon.svg';
+import blackHeart from '../../images/blackHeartIcon.svg';
 import './style.css';
+import handleFav from '../../services/handleFav';
+import handleCheck from '../../services/handleCheck';
+import checkRender from '../../services/CheckRender';
+import FinishBtn from '../../Components/FinishBtn';
+import CheckRender from '../../services/CheckRender';
+
+const copy = require('clipboard-copy');
 
 const RecipeInProgress = () => {
-  const [recipeInfo, setRecipeInfo] = useState([]);
+  const [recipeInfo, setRecipeInfo] = useState({});
+  const [body, setBody] = useState({});
+  const [fav, setFav] = useState(false);
+  const [shared, setShared] = useState(false);
   const { pathname } = useLocation();
   const { getAllRecipes } = useContext(Context);
   const [ingredientsCheck, setIngredientsCheck] = useState([]);
@@ -31,51 +44,54 @@ const RecipeInProgress = () => {
     arrayDetail();
   }, [pathname]);
 
-  const handleCheck = (ingredientAndMeasure) => {
-    console.log(storagePrev);
+  useEffect(() => {
+    const favListLS = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    const idIndex = -2;
+    if (favListLS && Object.keys(recipeInfo).includes('idMeal')) {
+      const isFav = favListLS
+        .some((info) => (info.id) === pathname.split('/').at(idIndex));
+      setFav(isFav);
+    }
+    if (favListLS && Object.keys(recipeInfo).includes('idDrink')) {
+      const isFav = favListLS
+        .some((info) => (info.id) === pathname.split('/').at(idIndex));
+      setFav(isFav);
+    }
 
-    if (!ingredientsCheck.includes(ingredientAndMeasure)) {
-      const body = {
-        ...storagePrev,
-        [cat]: {
-          ...storagePrev[cat],
-          [recipeId]: [...ingredientsCheck, ingredientAndMeasure],
-        },
+    const categoryLs = (Object.keys(recipeInfo).at(0));
+    if (categoryLs === 'idMeal') {
+      const foodBody = {
+        alcoholicOrNot: '',
+        category: recipeInfo.strCategory,
+        id: recipeInfo.idMeal,
+        image: recipeInfo.strMealThumb,
+        name: recipeInfo.strMeal,
+        nationality: recipeInfo.strArea,
+        type: 'food',
       };
-      localStorage.setItem('inProgressRecipes', JSON.stringify(body));
-      setIngredientsCheck([...ingredientsCheck, ingredientAndMeasure]);
-    }
-
-    if (ingredientsCheck.includes(ingredientAndMeasure)) {
-      setIngredientsCheck(ingredientsCheck
-        .filter((ingredient) => ingredient !== ingredientAndMeasure));
-      const body = {
-        ...storagePrev,
-        [cat]: {
-          ...storagePrev[cat],
-          [recipeId]: ingredientsCheck
-            .filter((ingredient) => ingredient !== ingredientAndMeasure),
-        },
+      setBody(foodBody);
+    } else {
+      const drinkBody = {
+        alcoholicOrNot: recipeInfo.strAlcoholic,
+        category: recipeInfo.strCategory,
+        id: recipeInfo.idDrink,
+        image: recipeInfo.strDrinkThumb,
+        name: recipeInfo.strDrink,
+        nationality: '',
+        type: 'drink',
       };
-      localStorage.setItem('inProgressRecipes', JSON.stringify(body));
+      setBody(drinkBody);
     }
-  };
+  }, [recipeInfo]);
 
-  const checkRender = (ingredientAndMeasure) => {
-    if (ingredientsCheck.includes(ingredientAndMeasure)) {
-      return (<input
-        onChange={ () => handleCheck(ingredientAndMeasure) }
-        type="checkbox"
-        checked
-      />
-      );
-    }
-
-    return (<input
-      onChange={ () => handleCheck(ingredientAndMeasure) }
-      type="checkbox"
-    />
-    );
+  const checking = (ingredientAndMeasure) => {
+    handleCheck({
+      setIngredientsCheck,
+      ingredientsCheck,
+      ingredientAndMeasure,
+      storagePrev,
+      cat,
+      recipeId });
   };
 
   useEffect(() => {
@@ -87,38 +103,27 @@ const RecipeInProgress = () => {
       setCat('cocktails');
       setRecipeId(recipeInfo.idDrink);
     }
-
     const ingredientsLS = JSON.parse(localStorage.getItem('inProgressRecipes'));
-
     if (ingredientsLS && Object.keys(recipeInfo)[0] === 'idMeal'
       && ingredientsLS.meals[recipeInfo.idMeal]) {
       setIngredientsCheck(ingredientsLS.meals[recipeInfo.idMeal]);
     }
-
     if (ingredientsLS && Object.keys(recipeInfo)[0] === 'idDrink'
       && ingredientsLS.cocktails[recipeInfo.idDrink]) {
       setIngredientsCheck(ingredientsLS.cocktails[recipeInfo.idDrink]);
     }
-
     if (!ingredientsLS) {
       const initialLS = { meals: {}, cocktails: {} };
       localStorage.setItem('inProgressRecipes', JSON.stringify(initialLS));
       setStoragePrev({ meals: {}, cocktails: {} });
     }
-
     setStoragePrev(ingredientsLS);
   }, [recipeInfo]);
 
-  useEffect(() => {
-    console.log(ingredientsCheck);
-  }, [ingredientsCheck]);
-
   return (
     <>
-      {/* <FinishRecipeBtn /> */}
-
+      <FinishBtn ingredientsCheck={ ingredientsCheck } recipeInfo={ recipeInfo } />
       <h1>em progresso</h1>
-
       {
         pathname.includes('foods') ? (
           <div>
@@ -130,11 +135,9 @@ const RecipeInProgress = () => {
             />
             <h1 data-testid="recipe-title">{recipeInfo.strMeal}</h1>
             <p data-testid="recipe-category">{recipeInfo.strCategory}</p>
-
           </div>
         ) : (
           <div>
-
             <img
               data-testid="recipe-photo"
               width="250px"
@@ -149,7 +152,45 @@ const RecipeInProgress = () => {
           </div>
         )
       }
-
+      <button
+        data-testid="share-btn"
+        type="button"
+        src={ imageComp }
+        onClick={ () => {
+          setShared(!shared);
+          copy(`http://localhost:3000/${pathname.split('/')[1]}/${pathname.split('/')[2]}`);
+        } }
+      >
+        <img src={ imageComp } alt="sla" />
+      </button>
+      { (fav) ? (
+        <button
+          data-testid="favorite-btn"
+          src={ blackHeart }
+          alt="blackheart"
+          type="button"
+          onClick={ () => {
+            handleFav(recipeInfo, body, setFav);
+            setFav(false);
+          } }
+        >
+          <img src={ blackHeart } alt="black" />
+        </button>
+      ) : (
+        <button
+          data-testid="favorite-btn"
+          src={ whiteHeart }
+          alt="whiteHeart"
+          type="button"
+          onClick={ () => {
+            handleFav(recipeInfo, body, setFav);
+            setFav(true);
+          } }
+        >
+          <img src={ whiteHeart } alt="white" />
+        </button>
+      )}
+      {shared && <p>Link copied!</p>}
       {
         Object.keys(recipeInfo)
           .filter((a) => a.includes('strIngredient')).map((info, index) => {
@@ -157,22 +198,27 @@ const RecipeInProgress = () => {
             const measureRecipe = `${recipeInfo[`strMeasure${index + 1}`]}`;
             const ingredientAndMeasure = ingredientRecipe + measureRecipe;
 
-            const thisCheck = checkRender(ingredientAndMeasure);
-            return (
-              <div
-                key={ `ingredient-${index}` }
-                data-testid={ `${index}-ingredient-step` }
-              >
-                {thisCheck}
-                <p>
-                  { recipeInfo[info]
-                && `${recipeInfo[info]} - ${recipeInfo[`strMeasure${index + 1}`]}`}
-                </p>
-              </div>
-            );
+            if (recipeInfo[info] && recipeInfo[info].length > 1) {
+              return (
+                <div
+                  key={ `ingredient-${index}` }
+                  data-testid={ `${index}-ingredient-step` }
+                >
+                  <CheckRender
+                    ingredientAndMeasure={ ingredientAndMeasure }
+                    ingredientsCheck={ ingredientsCheck }
+                    checking={ checking }
+                  />
+                  <p>
+                    { recipeInfo[info]
+                    && `${recipeInfo[info]} - ${recipeInfo[`strMeasure${index + 1}`]}`}
+                  </p>
+                </div>
+              );
+            }
+            return null;
           })
       }
-
       <p data-testid="instructions">{recipeInfo.strInstructions}</p>
       {
         recipeInfo.strYoutube ? (
@@ -197,9 +243,7 @@ const RecipeInProgress = () => {
           />
         )
       }
-
     </>
   );
 };
-
 export default RecipeInProgress;
