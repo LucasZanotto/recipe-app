0095057ae1585c17554de357c964b8f36cb9e35a
Rafael Lira <rafaelira347@gmail.com>
2022-07-21 15:41:49 -0300

    req51

--- src/pages/FavoriteRecipes/index.jsx
+++ src/pages/FavoriteRecipes/index.jsx
@@ -2,12 +2,62 @@ import React from 'react';
 import Header from '../../Components/Header';
 
 const FavoriteRecipes = () => {
-  const sla = 'sla';
+  const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+
   return (
     <>
       <Header />
       <h1>Favorite Recipes</h1>
-      <h1>{sla}</h1>
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+      >
+        All
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+      >
+        Food
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+      >
+        Drink
+      </button>
+      {favoriteRecipes.map((favoriteRecipe, index) => (
+        <div key={ index }>
+          <p
+            data-testid={ `${index}-horizontal-name` }
+          >
+            {favoriteRecipe.name}
+          </p>
+          <img
+            src={ favoriteRecipe.image }
+            alt="comida"
+            width={ 250 }
+            data-testid={ `${index}-horizontal-image` }
+          />
+          <p
+            data-testid={ `${index}-horizontal-top-text` }
+          >
+            {favoriteRecipe.name}
+          </p>
+          <button
+            type="button"
+            data-testid={ `${index}-horizontal-share-btn` }
+          >
+            Share
+          </button>
+          <button
+            type="button"
+            data-testid={ `${index}-horizontal-favorite-btn` }
+          >
+            Favorite
+          </button>
+        </div>
+      ))}
     </>
   );
 };
