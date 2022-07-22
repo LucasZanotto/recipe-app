9000fc4513343f14aa004f0dedc146403eed94be
Gabriel Gomes Rodrigues <gabrielbj99@gmail.com>
2022-07-22 11:59:48 -0300

    Requisito 55

--- src/pages/FavoriteRecipes/index.jsx
+++ src/pages/FavoriteRecipes/index.jsx
@@ -7,6 +7,7 @@ import blackHeart from '../../images/blackHeartIcon.svg';
 const FavoriteRecipes = () => {
   const [favoriteRecipes, setFavoriteRecipes] = useState([]);
   const [shared, setShared] = useState(false);
+  const [filter, setFilter] = useState('');
 
   useEffect(() => (
     setFavoriteRecipes(JSON.parse(localStorage.getItem('favoriteRecipes')))
@@ -24,67 +25,72 @@ const FavoriteRecipes = () => {
       <button
         type="button"
         data-testid="filter-by-all-btn"
+        onClick={ () => setFilter('') }
       >
         All
       </button>
       <button
         type="button"
         data-testid="filter-by-food-btn"
+        onClick={ () => setFilter('food') }
       >
         Food
       </button>
       <button
         type="button"
         data-testid="filter-by-drink-btn"
+        onClick={ () => setFilter('drink') }
       >
         Drink
       </button>
-      {favoriteRecipes && favoriteRecipes.map((favoriteRecipe, index) => (
-        <div key={ index }>
-          <img
-            src={ favoriteRecipe.image }
-            alt="comida"
-            width={ 250 }
-            data-testid={ `${index}-horizontal-image` }
-          />
-          <p
-            data-testid={ `${index}-horizontal-top-text` }
-          >
-            {`${favoriteRecipe.nationality} - ${favoriteRecipe.category}`}
-          </p>
-          <p
-            data-testid={ `${index}-horizontal-name` }
-          >
-            {favoriteRecipe.name}
-          </p>
-          { favoriteRecipe.alcoholicOrNot && (
-            <p
-              data-testid={ `${index}-horizontal-top-text` }
-            >
-              {favoriteRecipe.alcoholicOrNot}
-            </p>)}
-          <button
-            src={ imageComp }
-            type="button"
-            data-testid={ `${index}-horizontal-share-btn` }
-            onClick={ () => {
-              setShared((prevState) => (prevState ? '' : favoriteRecipe.name));
-              copy(`http://localhost:3000/foods/${favoriteRecipe.id}`);
-            } }
-          >
-            <img src={ imageComp } alt="sla" />
-          </button>
-          <button
-            src={ blackHeart }
-            type="button"
-            data-testid={ `${index}-horizontal-favorite-btn` }
-            onClick={ () => removeFavoriteRecipe(favoriteRecipe.id) }
-          >
-            <img src={ blackHeart } alt="sla" />
-          </button>
-          {shared === favoriteRecipe.name && <p>Link copied!</p>}
-        </div>
-      ))}
+      {favoriteRecipes
+        && favoriteRecipes.filter((recipe) => recipe.type.includes(filter))
+          .map((favoriteRecipe, index) => (
+            <div key={ index }>
+              <img
+                src={ favoriteRecipe.image }
+                alt="comida"
+                width={ 250 }
+                data-testid={ `${index}-horizontal-image` }
+              />
+              <p
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                {`${favoriteRecipe.nationality} - ${favoriteRecipe.category}`}
+              </p>
+              <p
+                data-testid={ `${index}-horizontal-name` }
+              >
+                {favoriteRecipe.name}
+              </p>
+              { favoriteRecipe.alcoholicOrNot && (
+                <p
+                  data-testid={ `${index}-horizontal-top-text` }
+                >
+                  {favoriteRecipe.alcoholicOrNot}
+                </p>)}
+              <button
+                src={ imageComp }
+                type="button"
+                data-testid={ `${index}-horizontal-share-btn` }
+                onClick={ () => {
+                  setShared((prevState) => (prevState ? '' : favoriteRecipe.name));
+                  copy(`http://localhost:3000/foods/${favoriteRecipe.id}`);
+                } }
+              >
+                <img src={ imageComp } alt="sla" />
+              </button>
+              <button
+                src={ blackHeart }
+                type="button"
+                data-testid={ `${index}-horizontal-favorite-btn` }
+                onClick={ () => removeFavoriteRecipe(favoriteRecipe.id) }
+              >
+                <img src={ blackHeart } alt="sla" />
+              </button>
+              {shared === favoriteRecipe.name && <p>Link copied!</p>}
+            </div>
+          ))}
     </>
   );
 };
