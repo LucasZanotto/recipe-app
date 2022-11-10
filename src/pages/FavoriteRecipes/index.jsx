72b7264102582ad6a6ed85f3dcf208ef7d27a13a
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2022-11-10 20:40:31 -0300

    feat: css favorite

--- src/pages/FavoriteRecipes/index.jsx
+++ src/pages/FavoriteRecipes/index.jsx
@@ -4,16 +4,15 @@ import { Link } from 'react-router-dom';
 import Header from '../../Components/Header';
 import imageComp from '../../images/shareIcon.svg';
 import blackHeart from '../../images/blackHeartIcon.svg';
+import './style.css';
 
 const FavoriteRecipes = () => {
   const [favoriteRecipes, setFavoriteRecipes] = useState([]);
   const [shared, setShared] = useState(false);
   const [filter, setFilter] = useState('');
-
   useEffect(() => (
     setFavoriteRecipes(JSON.parse(localStorage.getItem('favoriteRecipes')))
   ), []);
-
   const removeFavoriteRecipe = (recipeId) => {
     const newFavoriteRecipes = favoriteRecipes.filter(({ id }) => id !== recipeId);
     setFavoriteRecipes(newFavoriteRecipes);
@@ -21,82 +20,98 @@ const FavoriteRecipes = () => {
   };
 
   return (
-    <>
+    <div className="favorite-container">
       <Header />
-      <button
-        type="button"
-        data-testid="filter-by-all-btn"
-        onClick={ () => setFilter('') }
-      >
-        All
-      </button>
-      <button
-        type="button"
-        data-testid="filter-by-food-btn"
-        onClick={ () => setFilter('food') }
-      >
-        Food
-      </button>
-      <button
-        type="button"
-        data-testid="filter-by-drink-btn"
-        onClick={ () => setFilter('drink') }
-      >
-        Drink
-      </button>
-      {favoriteRecipes
+      <div className="favorite-filters">
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+          onClick={ () => setFilter('') }
+        >
+          All
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-food-btn"
+          onClick={ () => setFilter('food') }
+        >
+          Food
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+          onClick={ () => setFilter('drink') }
+        >
+          Drink
+        </button>
+      </div>
+      <div className="cards-group-container">
+        {favoriteRecipes
         && favoriteRecipes.filter((recipe) => recipe.type.includes(filter))
           .map((favoriteRecipe, index) => (
-            <div key={ index }>
-              <Link to={ `${favoriteRecipe.type}s/${favoriteRecipe.id}` }>
-                <img
-                  src={ favoriteRecipe.image }
-                  alt="comida"
-                  width={ 250 }
-                  data-testid={ `${index}-horizontal-image` }
-                />
-              </Link>
-              <p
-                data-testid={ `${index}-horizontal-top-text` }
-              >
-                {`${favoriteRecipe.nationality} - ${favoriteRecipe.category}`}
-              </p>
-              <Link to={ `${favoriteRecipe.type}s/${favoriteRecipe.id}` }>
-                <p
-                  data-testid={ `${index}-horizontal-name` }
-                >
-                  {favoriteRecipe.name}
-                </p>
-              </Link>
-              { favoriteRecipe.alcoholicOrNot && (
+            <div
+              className="favorite-card"
+              key={ index }
+            >
+              <div className="image-container">
+                <Link to={ `${favoriteRecipe.type}s/${favoriteRecipe.id}` }>
+                  <img
+                    className="food-image"
+                    src={ favoriteRecipe.image }
+                    alt="comida"
+                    width={ 250 }
+                    data-testid={ `${index}-horizontal-image` }
+                  />
+                </Link>
+              </div>
+              <div className="favorite-context">
+                <Link to={ `${favoriteRecipe.type}s/${favoriteRecipe.id}` }>
+                  <p
+                    data-testid={ `${index}-horizontal-name` }
+                  >
+                    {favoriteRecipe.name}
+                  </p>
+                </Link>
                 <p
                   data-testid={ `${index}-horizontal-top-text` }
                 >
-                  {favoriteRecipe.alcoholicOrNot}
-                </p>)}
-              <button
-                src={ imageComp }
-                type="button"
-                data-testid={ `${index}-horizontal-share-btn` }
-                onClick={ () => {
-                  setShared((prevState) => (prevState ? '' : favoriteRecipe.name));
-                  copy(`http://localhost:3000/foods/${favoriteRecipe.id}`);
-                } }
-              >
-                <img src={ imageComp } alt="sla" />
-              </button>
-              <button
-                src={ blackHeart }
-                type="button"
-                data-testid={ `${index}-horizontal-favorite-btn` }
-                onClick={ () => removeFavoriteRecipe(favoriteRecipe.id) }
-              >
-                <img src={ blackHeart } alt="sla" />
-              </button>
-              {shared === favoriteRecipe.name && <p>Link copied!</p>}
+                  {`${favoriteRecipe.nationality} - ${favoriteRecipe.category}`}
+                </p>
+                { favoriteRecipe.alcoholicOrNot && (
+                  <p
+                    data-testid={ `${index}-horizontal-top-text` }
+                  >
+                    {favoriteRecipe.alcoholicOrNot}
+                  </p>)}
+                <div className="favorite-btn">
+
+                  <button
+                    src={ blackHeart }
+                    type="button"
+                    data-testid={ `${index}-horizontal-favorite-btn` }
+                    onClick={ () => removeFavoriteRecipe(favoriteRecipe.id) }
+                  >
+                    <img src={ blackHeart } alt="sla" />
+                  </button>
+                  <button
+                    src={ imageComp }
+                    type="button"
+                    data-testid={ `${index}-horizontal-share-btn` }
+                    onClick={ () => {
+                      setShared((prevState) => (prevState ? '' : favoriteRecipe.name));
+                      copy(`http://localhost:3000/foods/${favoriteRecipe.id}`);
+                    } }
+                  >
+                    <img src={ imageComp } alt="sla" />
+                  </button>
+                  {shared === favoriteRecipe.name && <p>Link copied!</p>}
+
+                </div>
+              </div>
             </div>
           ))}
-    </>
+      </div>
+    </div>
   );
 };
 
